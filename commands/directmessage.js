1624d6ee5f50bd01ad99c44292d23184222f248b
Sympleque <xamser7@gmail.com>
2021-05-13 13:29:42 -0500

    Directmessaging bug fixes and improvements

--- commands/directmessage.js
+++ commands/directmessage.js
@@ -3,23 +3,48 @@ module.exports = {
 	name: 'directmessage',
 	aliases: ['dm'],
 	description: 'Messages mentioned guild members with the specified message',
-	usage: '<USER...> <MESSAGE>',
-	args: true,
+	usage: '<USER...> [OPTIONS] <MESSAGE>',
 	guild: true,
 	admin: true,
 
+	flags: [
+		{
+			name: 'everyone',
+			aliases: [ 'e' ],
+			description: 'Sends the message to everyone in the server'
+		},
+		{
+			name: 'message',
+			aliases: [ 'm' ],
+			description: 'Message to send to the specified members'
+		},
+	],
+
 	async execute(msg, body) 
 	{
-		let announcement = body.args[body.args.length - 1];
+		let announcement = undefined;
+		
+		if (body.flags.message)
+			announcement = body.flags.message[0];
 
-		if (msg.mentions.everyone)
+		else if (body.args.length > 0)
+			announcement = body.args[body.args.length - 1];
+
+		if (!announcement)
+		{
+			msg.channel.send("You must specify the message as an argument or using the -m flag!");
+			return;
+		}
+
+		if (msg.mentions.everyone || body.flags.everyone)
 		{
 			// everyone tag does not list all members
-			Broadcast(msg.guild.members, announcement);
+			Broadcast(msg.guild.members.cache, announcement);
 			return;
 		}
 
 		Broadcast(msg.mentions.members, announcement);
+		msg.react('👌');
 	},
 };
 
@@ -30,8 +55,6 @@ async function Broadcast(members, announcement)
 		if (member.user.bot)
 			continue;
 		
-		// Must await to wait for channel handle
-		let channel = await member.createDM();
-		channel.send(announcement);
+		member.createDM().then(channel => channel.send(announcement));
 	}
 }

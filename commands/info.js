d16c33acc4796301f47f685dde0de6ff64979618
Sympleque <xamser7@gmail.com>
2021-05-12 22:57:42 -0500

    Updated info command

--- commands/info.js
+++ commands/info.js
@@ -1,16 +1,29 @@
 
+const { MessageEmbed } = require('discord.js');
+
 module.exports = {
 
 	name: 'info',
 	aliases: ['guild', 'guild-info', 'server', 'server-info'],
-	description: 'States all of the information relating to the current guild',
+	description: 'States all of the information relating to the server',
 	args: false,
 	guild: true,
 
 	async execute(msg) 
 	{
-		msg.channel.send(
-			`Server name: **${msg.guild.name}**\nCreated: ${msg.guild.createdAt}\nTotal members: ${msg.guild.memberCount}`
-		);
+		const guild = msg.guild;
+
+		const embed = new MessageEmbed()
+			.setColor(0x5cb3ff)
+			.setTitle(guild.name)
+			.setURL('https://discord.gg/pNrzXpS')
+			.setDescription(guild.description || '')
+			.setThumbnail(guild.iconURL())
+			.addFields(
+				{ name: 'Total Members', value: guild.memberCount },
+				{ name: 'Created', value: guild.createdAt }
+			);
+
+		msg.channel.send(embed);
 	},
 };

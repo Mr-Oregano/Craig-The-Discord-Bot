fabd3ce7340490c1993c37d9e24609fbc8ae72be
Spice <kf.shared.ptr@gmail.com>
2021-10-09 21:16:33 -0500

    Added join command.

---
+++ commands/music/join.js
@@ -0,0 +1,22 @@
+
+module.exports = {
+
+	name: 'join',
+	aliases: ['j', 'connect'],
+	description: 'Join the vc (must be in a voice channel to use).',
+	args: false,
+	guild: true,
+
+	execute(msg, cmd) 
+	{
+		const vc = msg.member.voice.channel;
+
+		if (!vc)
+		{
+			msg.reply("You need to be in a voice channel you dumb fuck.");
+			return;
+		}
+
+		vc.join();
+	}
+};
